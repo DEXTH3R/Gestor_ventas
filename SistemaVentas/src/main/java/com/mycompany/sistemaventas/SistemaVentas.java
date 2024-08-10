e37d932c39073cd577d4db273c70472058a7a2d3
Jorgy Rios <Jorgy995@gmail.com>
2024-08-10 18:07:19 +0200

    He creado la vista del login y he puesto las imagenes modificadas para el proyecto

---
+++ SistemaVentas/src/main/java/com/mycompany/sistemaventas/SistemaVentas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemaventas;
+
+/**
+ *
+ * @author JORGY
+ */
+public class SistemaVentas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
